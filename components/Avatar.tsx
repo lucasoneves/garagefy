42894b3ddbff2e06d80ac1421aed996ed3ef30a8
lucasoneves <casluhc@gmail.com>
2026-03-06 21:10:35 -0300

    First commit

---
+++ components/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Avatar({ src, alt, username }: { src: string; alt: string; username: string }) {
+  return (
+    <div className="user-avatar relative w-full flex items-center gap-2">
+      <Image
+        src={src}
+        alt={alt}
+        width={48}
+        height={48}
+        className="rounded-lg"
+        priority
+      />
+      <span className="is-online absolute bottom-0">
+        <span className="dot rounded-full w-2 h-2 bg-green-500 block" />
+      </span>
+      <span className="text-white text-sm">{username}</span>
+    </div>
+  );
+}
